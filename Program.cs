f82c3cc7069b6c80242b119cdfa8e574a14f3038
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-15 14:28:15 -0700

    basic c# console application

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Survey
+{
+
+    class Data {
+        public string Name;
+        public int Age;
+        public string Month;
+
+        public void Display() 
+        {
+            Console.WriteLine("Your name is: {0}", Name);
+            Console.WriteLine("Your age is: {0}", Age);
+            Console.WriteLine("Your birth month is: {0}", Month);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Data();
+
+            Console.WriteLine("What is your name?");
+            data.Name = TryAnswer();
+
+            Console.WriteLine("What is your age?");
+            data.Age = int.Parse(TryAnswer());
+
+            Console.WriteLine("What month were you born in?");
+            data.Month = TryAnswer();
+
+            data.Display();
+
+        }
+
+        static string TryAnswer()
+        {
+            var question = Console.ReadLine();
+            if(question == "")
+            {
+                Console.WriteLine("You didn't input anything. Please try again.");
+                return Console.ReadLine();
+            }
+                return question;
+        }
+    }
+}
